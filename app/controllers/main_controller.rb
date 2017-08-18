fa07e358a4a60fb7c57c0a585f79024a9e5b0283
임채민 <coals1153@gmail.com>
2017-08-19 03:20:02 +0900

    regex

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -31,25 +31,27 @@ class MainController < ApplicationController
     @result = findItems(keys)
   end
 
-
-  def ocr(image)
-    # 경완
-    # return string
-  end
-
-
-
-
   def getKeys(text)
-      #array
-
-
+      #topten10
+    keys = ""
+    if not /([A-Z]){3}\d([A-Z]){2}\d{4}/.match(text).nil?
+      keys = /([A-Z]){3}\d([A-Z]){2}\d{4}/.match(text)[0]
+      #유니클로
+    elsif not /\d{3}-\d{6}/.match(text).nil?
+      keys = /\d{3}-\d{6}/.match(text)[0]
+      #아디다스
+    elsif not /\b[(A-Z)][A-Z|\d]\d{4}\b/.match(text).nil?
+      keys = /\b[(A-Z)][A-Z|\d]\d{4}\b/.match(text)[0]
+      #나이키
+    elsif not /\d{6}( |-)\d{3}/.match(text).nil?
+      keys = /\d{6}( |-)\d{3}/.match(text)[0]
+    end
   end
 
   def findItems(keys)
 
     # 경완 채민
-    uri = URI.parse("https://openapi.naver.com/v1/search/shop.json?query=852459&display=10&start=1&sort=sim")
+    uri = URI.parse("https://openapi.naver.com/v1/search/shop.json?query=#{keys}&display=10&start=1&sort=sim")
     request = Net::HTTP::Get.new(uri)
     request["X-Naver-Client-Id"] = "FfNTPfd3q0P2hp7qfAoW"
     request["X-Naver-Client-Secret"] = "WzmJjLASgI"
@@ -65,6 +67,7 @@ class MainController < ApplicationController
     # return an array of results
     # puts response_result
     search_result = JSON.parse(response_result)
+    puts search_result["items"]
     return search_result["items"]
 
     # puts items"items"
