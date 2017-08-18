84b581cb19b963c8ec3cdff18a830f812c442e7c
HyunWoo <chaosphch@gmail.com>
2017-08-18 19:34:52 +0900

    routes, default controllers, views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'search/index'
+
+  get 'main/index'
+
+  get 'main/search'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'main/index'
+  get 'main/search'
+  get 'card/index'
+  get 'mypage/index'
+
 end
