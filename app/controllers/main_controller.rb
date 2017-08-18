990cd1e64ea8554b56e324a54166210cfb01c3ae
KyoungWan <opera5@snu.ac.kr>
2017-08-19 03:25:56 +0900

    auction_search

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,12 @@
 require 'google/cloud/vision'  
 require 'googleauth'           
-
+require 'nokogiri'
+require 'open-uri'
 
 class MainController < ApplicationController
   def index
   end
+
 
   def ocr(file)
     # Google Vision API                                                                                                   
@@ -19,8 +21,8 @@ class MainController < ApplicationController
   end
 
   def put_image(image)
-   # file_data = params[:file]
-   # @result = ocr(file_data.path)                                                                                         
+    # file_data = params[:file]
+    # @result = ocr(file_data.path)                                                                                         
   end
 
 
@@ -28,8 +30,27 @@ class MainController < ApplicationController
   def search
     image = params[:image]
     # Do something with image
-   # put_image(image)
-   @result = ocr(image.path)
-   render "put_image"
+    # put_image(image)
+    @result = ocr(image.path)
+    render "put_image"
   end
+
+  def auction
+    doc = Nokogiri::HTML(open('http://search.auction.co.kr/search/search.aspx?keyword=MSX2PP1008&itemno=&nickname=&frm=hometab&dom=auction&isSuggestion=No&retry=&Fwk=MSX2PP1008&acode=SRP_SU_0100&arraycategory=&encKeyword=MSX2PP1008'))
+
+    itemArray = Array.new
+    list_view = doc.css(".list_view")
+    list_view.each do |x|
+      item = Hash.new
+      item[:image]=x['src']
+      item[:title]=x.css('.item_title').text
+      item[:price]=x.css('.item_price strong').text
+
+
+      puts "what i want to show"
+
+      itemArray.push(item)
+    end
+  end
+
 end
