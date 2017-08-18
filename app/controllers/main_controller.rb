61fca4e27fcedaef85cefa64933798a497a51662
Jaeseong Seo <07js23@gmail.com>
2017-08-18 23:15:32 +0900

    Functions to implement

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -10,7 +10,23 @@ class MainController < ApplicationController
     image = params[:image]
     # Do something with image
 
+    text = ocr(image)
+    keys = getKeys(text)
+    @result = findItems(keys)
+  end
 
+  def ocr(image)
+    # 경완
+    # return string
+  end
+
+  def getKeys(text)
+    # 채민
+    # return an array of strings
+  end
+
+  def findItems(keys)
+    # 경완 채민
     uri = URI.parse("https://openapi.naver.com/v1/search/shop.xml?query=%EC%A3%BC%EC%8B%9D&display=10&start=1&sort=sim")
     request = Net::HTTP::Get.new(uri)
     request["X-Naver-Client-Id"] = "{A8B3TPKsS8UbC2fWfagt}"
@@ -26,6 +42,7 @@ class MainController < ApplicationController
 
     # response.code
     # response.body
-
+    # return an array of results
   end
+
 end
