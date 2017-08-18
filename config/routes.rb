08f5ccd065dae8bdddd8920df6775bb87770c7c7
HyunWoo <chaosphch@gmail.com>
2017-08-18 19:37:34 +0900

    root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get 'search/index'
-
-  get 'main/index'
-
-  get 'main/search'
-
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main/index'
 
   get 'main/index'
   get 'main/search'
   get 'card/index'
   get 'mypage/index'
 
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
